5dd257afe2e48a49a48141d21d932d7bc9e392b5
Luis Gerbelli <luis.ricardo.gerbelli@gmail.com>
2021-07-04 12:04:42 -0300

    Availability added

--- js/script.js
+++ js/script.js
@@ -15,13 +15,27 @@ function loadMain(file_json) {
       resp += '<div class="item'
       if (file_json[room]['Item'][item]['Action'] === 'Reservado') resp += ' reservado'
       if (file_json[room]['Item'][item]['Action'] === 'Vendido') resp += ' vendido'
-      resp += '"><div class="item-info"><div class="item-key"><div>Código</div><div>Item</div><div>Quantidade</div><div>Altura</div><div>Largura</div><div>Profund.</div><div>Status</div><div>Sugestão</div></div><div class="item-value"><div>'
+      resp += '"><div class="item-info"><div class="item-key"><div>Código</div><div class="larger">Item</div><div>Status</div><div>Sugestão</div><div>Disponib.</div></div><div class="item-value"><div>'
       resp += item.substring(3, 8)
-      resp += '</div><div title="'
+      resp += '</div><div  class="larger" title="'
       resp += file_json[room]['Item'][item]['Name']
       resp += '">'
       resp += file_json[room]['Item'][item]['Name']
       resp += '</div><div>'
+      resp += file_json[room]['Item'][item]['Action']
+      resp += '</div><div>R$ '
+      resp += file_json[room]['Item'][item]['Price']
+      resp += '</div><div>'
+      resp += file_json[room]['Item'][item]['Availability']
+      resp += '</div></div></div><div class="item-picture">'
+      for (let p = Number(file_json[room]['Item'][item]['PictureMin']); p <= Number(file_json[room]['Item'][item]['PictureMax']); p++) {
+        resp += '<a href="./img/picture/'
+        resp += item + '_' + p
+        resp += '.JPG" target="_blank"><img src="./img/thumbnail/'
+        resp += item + '_' + p
+        resp += '.JPG"></a>'
+      }
+      resp += '</div><div class="item-note"><div class="item-key"><div>Quantidade</div><div>Altura</div><div>Largura</div><div>Profund.</div><div>Avaria</div></div><div class="item-value"><div>'
       resp += file_json[room]['Item'][item]['Quantity']
       resp += '</div><div>'
       resp += file_json[room]['Item'][item]['Height']
@@ -32,19 +46,7 @@ function loadMain(file_json) {
       resp += '</div><div>'
       resp += file_json[room]['Item'][item]['Depth']
       if (file_json[room]['Item'][item]['Depth'] !== '-') resp += 'cm'
-      resp += '</div><div>'
-      resp += file_json[room]['Item'][item]['Action']
-      resp += '</div><div>R$ '
-      resp += file_json[room]['Item'][item]['Price']      
-      resp += '</div></div></div><div class="item-picture">'
-      for (let p = Number(file_json[room]['Item'][item]['PictureMin']); p <= Number(file_json[room]['Item'][item]['PictureMax']); p++) {
-        resp += '<a href="./img/picture/'
-        resp += item + '_' + p
-        resp += '.JPG" target="_blank"><img src="./img/thumbnail/'
-        resp += item + '_' + p
-        resp += '.JPG"></a>'
-      }
-      resp += '</div><div class="item-note"><div class="item-key"><div>Avaria</div></div><div class="item-value"><div title="'
+      resp += '</div><div title="'
       resp += file_json[room]['Item'][item]['Damage']
       resp += '">'
       resp += file_json[room]['Item'][item]['Damage']
