272f066bed9c0f95ba90d019d8abca20d3b45882
Luis Gerbelli <luis.ricardo.gerbelli@gmail.com>
2021-07-04 12:14:39 -0300

    Look-and-feel improvement

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function loadMain(file_json) {
       resp += '<div class="item'
       if (file_json[room]['Item'][item]['Action'] === 'Reservado') resp += ' reservado'
       if (file_json[room]['Item'][item]['Action'] === 'Vendido') resp += ' vendido'
-      resp += '"><div class="item-info"><div class="item-key"><div>Código</div><div class="larger">Item</div><div>Status</div><div>Sugestão</div><div>Disponib.</div></div><div class="item-value"><div>'
+      resp += '"><div class="item-info"><div class="item-key"><div>Código</div><div class="larger">Item</div><div>Status</div><div>Disponib.</div><div>Sugestão</div></div><div class="item-value"><div>'
       resp += item.substring(3, 8)
       resp += '</div><div  class="larger" title="'
       resp += file_json[room]['Item'][item]['Name']
@@ -23,10 +23,10 @@ function loadMain(file_json) {
       resp += file_json[room]['Item'][item]['Name']
       resp += '</div><div>'
       resp += file_json[room]['Item'][item]['Action']
-      resp += '</div><div>R$ '
-      resp += file_json[room]['Item'][item]['Price']
       resp += '</div><div>'
       resp += file_json[room]['Item'][item]['Availability']
+      resp += '</div><div>R$ '
+      resp += file_json[room]['Item'][item]['Price']      
       resp += '</div></div></div><div class="item-picture">'
       for (let p = Number(file_json[room]['Item'][item]['PictureMin']); p <= Number(file_json[room]['Item'][item]['PictureMax']); p++) {
         resp += '<a href="./img/picture/'
@@ -35,7 +35,7 @@ function loadMain(file_json) {
         resp += item + '_' + p
         resp += '.JPG"></a>'
       }
-      resp += '</div><div class="item-note"><div class="item-key"><div>Quantidade</div><div>Altura</div><div>Largura</div><div>Profund.</div><div>Avaria</div></div><div class="item-value"><div>'
+      resp += '</div><div class="item-note"><div class="item-key"><div>Quantid.</div><div>Altura</div><div>Largura</div><div>Profund.</div><div>Avaria</div></div><div class="item-value"><div>'
       resp += file_json[room]['Item'][item]['Quantity']
       resp += '</div><div>'
       resp += file_json[room]['Item'][item]['Height']
